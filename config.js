8d9dde5be4ec79ed76fc3ca9fec358e77a4d2bfd
YecitPepe <99607946+YecitPepe@users.noreply.github.com>
2026-01-05 00:59:41 -0400

    version inicial de mi web

---
+++ config.js
@@ -0,0 +1,11 @@
+const SIDEBAR_LINKS = [
+  { title: "Redimension Por Lotes", url: "redimension.html" },
+  { title: "Giro Por Lotes", url: "girar.html" },
+  { title: "Recorte Aspecto Por Lotes", url: "recorteaspect.html" },
+  { title: "Removedor fondo Personas", url: "backgroundperson.html" },
+  { title: "Removedor fondo Objetos", url: "background.html" }
+];
+
+const SUPPORT_LINKS = [
+  { name: "Ko-fi", url: "https://ko-fi.com/ppluna", icon: "kofi" }
+];
